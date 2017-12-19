476cadc6c041767aed35eb1a8775c47fa939688c
Kriegsmarine13 <rediSKA195@mail.ru>
2017-12-19 17:54:55 +0300

    v0.1e rainbow
    ---ОБЩЕЕ---
    -Добавлена rainbow библиотека для подсветки кода

--- resources/views/layouts/main_page.blade.php
+++ resources/views/layouts/main_page.blade.php
@@ -6,6 +6,8 @@
         <link href="/resources/css/styles.css" rel="stylesheet" type="text/css">
         <script src="/resources/js/jquery-3.2.1.min.js"></script>
         <script src="/resources/js/masonry.pkgd.min.js" type="text/javascript"></script>
+        <script src="/resources/js/rainbow-custom.min.js"></script>
+        <link href="/resources/css/rainbow/all-hallows-eve.css" type="text/css" rel="stylesheet">
         @yield('metrics')
     </head>
     <body>
